06c630c6b2954c198a092028ff46e7279ab88e23
jackser143 <139695706+jackser143@users.noreply.github.com>
2024-04-03 20:37:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "",
-  SESSION_NAME: process.env.SESSION_NAME || "Bot~"
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://Ammu:<0t7JjPZ3AFF8O0Jj>@cluster0.zbstpkm.mongodb.net/",
+  SESSION_NAME: process.env.SESSION_NAME || "Kichappan~"
 };
